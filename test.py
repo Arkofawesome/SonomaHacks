cdf1469cddcb6ec4fb597b8465b285fbbf588b0c
xyz3r7 <XyZ3r7@outlook.com>
2025-07-05 15:19:54 -0400

    test version

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ from openai import OpenAI
 
 client = OpenAI(
 	api_key="lm-studio",  # 固定写法
-	base_url="http://localhost:1234/v1"
+	base_url="http://10.0.0.62:1234/v1"
 )
 
 response = client.chat.completions.create(
 	model="google/gemma-3-12b",
 	messages=[
 		{"role": "system", "content": "You are a useful ai"},
-		{"role": "user", "content": "tell me a joke"}
+		{"role": "user", "content": "To C or not to C that is the question"}
 	],
 	temperature=0.7,
 	stream=False
